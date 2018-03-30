a0d3fae6aa65f82c913fb8f18a5cda5603c4fda4
jairorjuela <genius85862@gmail.com>
2018-03-29 20:03:17 -0500

    Count Characters

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+     super();
+       this.state = {
+           chars_left: 0
+       }
+  }
+ handleChange(event) {
+     var input = event.target.value;
+     this.setState({
+         chars_left: input.length - 0
+     });
+ }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea onChange={this.handleChange.bind(this)} rows="3"></textarea>
+        <div className="counter">{this.state.chars_left}</div>
       </div>
     );
   }
