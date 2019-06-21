90e492a09d74bd7518c14b5dca097b52d75a16f6
jairorjuela <genius85862@gmail.com>
2019-06-20 23:00:49 -0500

    Change for Hooks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-class App extends Component {
-  constructor() {
-     super();
-       this.state = {
-           chars_left: 0
-       }
-  }
- handleChange(event) {
-     var input = event.target.value;
-     this.setState({
-         chars_left: input.length - 0
-     });
- }
-  render() {
-    return (
-      <div className="container">
-        <textarea onChange={this.handleChange.bind(this)} rows="3"></textarea>
-        <div className="counter">{this.state.chars_left}</div>
-      </div>
-    );
-  }
-}
+export const App = () => {
+
+  const [char, setChar] = useState(0)
+
+  return (
+    <div className="container">
+      <textarea onChange={e => setChar(e.target.value.length)} rows="3"></textarea>
+      <div className="counter">{char}</div>
+    </div>
+  );
+
+};
 
 export default App;
